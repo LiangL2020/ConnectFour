91557e97381292297748e30f35c8710b4527a368
Uma Pai <uma_paithankar@student.wayland.k12.ma.us>
2018-05-31 11:37:55 -0400

    added Piece class added paint Component to + made Piece objects (for testing) in Main

--- src/ConnectFourMain.java
+++ src/ConnectFourMain.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Created by student on 5/30/18.
+ * Created by Liang Lu, Uma Paithankar, & Megan Sin on 5/30/18.
  */
-public class ConnectFourMain {
+public class ConnectFourMain extends JPanel {
+    private int width, height;
+    public ConnectFourMain(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    Piece a = new Piece("blue");
+    Piece b = new Piece ("red");
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        a.draw(g2);
+        b.draw(g2);
+    }
+
+
 }
+
