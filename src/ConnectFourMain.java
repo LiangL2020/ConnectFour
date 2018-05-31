c4d73e77d6bc3f5ccbdd985556b97ece28bb56c9
Liang Lu <liang_lu@student.wayland.k12.ma.us>
2018-05-31 10:58:31 -0400

    added stuff in main - constructor - didWin - didTie - main
    all not finished

--- src/ConnectFourMain.java
+++ src/ConnectFourMain.java
@@ -1,5 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by student on 5/30/18.
  */
-public class ConnectFourMain {
+public class ConnectFourMain extends JPanel {
+    //instance fields
+    private int[][] grid = new int[6][7];
+
+    //constructor
+    public ConnectFourMain(){
+        grid = grid;
+    }
+
+    //check if win (4 in a row)
+    public boolean didWin(){
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+
+            }
+        }
+    }
+
+    //return true if didWin is false && everything is filled up
+    public boolean didTie(){
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Connect 4");
+        frame.setDefaultCloseOperation(3);
+        int width = 800;
+        int height = 800; //short?
+        frame.setPreferredSize(new Dimension(width, height + 24));
+        ConnectFourMain panel = new ConnectFourMain(width, height);
+        panel.setFocusable(true);
+        panel.grabFocus();
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
