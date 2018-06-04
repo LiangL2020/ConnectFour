0d37b8e133113ad20209429afeeab97683618c86
Uma Pai <uma_paithankar@student.wayland.k12.ma.us>
2018-06-04 13:31:47 -0400

    commented non working methods inmain

--- src/ConnectFourMain.java
+++ src/ConnectFourMain.java
@@ -49,24 +49,24 @@ public class ConnectFourMain extends JPanel {
 
     //TODO: do we need this???
     //check if 4 in a row
-    public boolean checkRow(int r){
-
-    }
-
-    //check if 4 in a col
-    public boolean checkCol(int c){
-
-    }
-
-    //check if 4 in diagonal
-    public boolean checkDiag(int d){
-
-    }
-
-    //return true if didWin is false && everything is filled up
-    public boolean didTie(){
-
-    }
+//    public boolean checkRow(int r){
+//
+//    }
+//
+//    //check if 4 in a col
+//    public boolean checkCol(int c){
+//
+//    }
+//
+//    //check if 4 in diagonal
+//    public boolean checkDiag(int d){
+//
+//    }
+//
+//    //return true if didWin is false && everything is filled up
+//    public boolean didTie(){
+//
+//    }
 
     public void paintComponent(Graphics g) {
         Graphics2D g2= (Graphics2D) g;
